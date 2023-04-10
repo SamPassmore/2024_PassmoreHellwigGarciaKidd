02f4972421634bbadeac6e3e2327e70bcc2f2b87
Sam Passmore <passmore.sam@gmail.com>
2023-04-10 17:14:06 +1000

    got plot kind of working

--- get_endangerment.R
+++ get_endangerment.R
@@ -6,4 +6,7 @@ language_ranges = readRDS("data/lang_polys_merged_cea.rds")
 
 df = data.frame(language_ranges)
 
-write.csv(df, "data/language_endangerment.csv")
+keep_vars = c("LANG_ISO", "LMP_POP1", "AES", "EGIDS", "L1_POP")
+df = df[,keep_vars]
+
+write.csv(df, "data/language_endangerment.csv", row.names = FALSE)
